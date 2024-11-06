7a2c870dc38fd951463932ba2510786eaf6bea11
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-06 23:40:32 +0100

    Update script.js

---
+++ code_structure/script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 800;
+
+class Game {
+  constructor() {
+    this.enemies = [];
+  }
+
+  update() {}
+
+  draw() {}
+
+  _addNewEnemy() {}
+}
+
+class Enemy {
+  constructor() {}
+
+  update() {}
+
+  draw() {}
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
