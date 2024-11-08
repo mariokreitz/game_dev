b4d0c2a22b7ca9a5abd4bf1f41101ded6d03c506
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-08 08:42:43 +0100

    Update player.class.js

--- state_management/classes/player.class.js
+++ state_management/classes/player.class.js
@@ -1,17 +1,29 @@
 export default class Player {
   constructor(gameWidth, gameHeight) {
-    this.gameWidth = gameHeight;
+    this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
     this.state = [];
     this.currentState = this.state[0];
     this.image = document.getElementById("dogImage");
     this.width = 200;
     this.height = 181.83;
-    this.x = 0;
-    this.y = 0;
+    this.x = this.gameWidth / 2 - this.width / 2;
+    this.y = this.gameHeight - this.height;
+    this.frameX = 0;
+    this.frameY = 0;
   }
 
   draw(ctx) {
-    ctx.drawImage(this.image, this.x, this.y);
+    ctx.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
