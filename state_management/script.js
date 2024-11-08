d727a39bc797c0866de495ab2f3881698f82e31d
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-08 08:32:14 +0100

    Update script.js

---
+++ state_management/script.js
@@ -0,0 +1,15 @@
+import Player from "./classes/player.class.js";
+
+window.addEventListener("load", function () {
+  const loading = document.getElementById("loading");
+  loading.classList.add("d_none");
+
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
+});
