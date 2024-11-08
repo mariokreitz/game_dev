2205e848994a7c4491bacc836dd29b2a21758e63
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-08 12:41:01 +0100

    Update utils.class.js

--- state_management/classes/utils.class.js
+++ state_management/classes/utils.class.js
@@ -1,3 +1,9 @@
+/**
+ * Draws the status text on the canvas.
+ * @param {CanvasRenderingContext2D} ctx - The 2D drawing context of the canvas.
+ * @param {Object} input - The input object with the last key.
+ * @param {Player} player - The player object with the current state.
+ */
 export function drawStatusText(ctx, input, player) {
   ctx.font = "20px Helvetica";
   ctx.fillText(`Last input: ${input.lastKey}`, 20, 50);
